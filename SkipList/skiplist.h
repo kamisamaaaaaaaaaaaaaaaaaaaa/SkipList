8afabf6d9ec81e2666c252277d1776deb5a74a2f
lys <1136433970@qq.com>
2023-02-22 15:30:57 +0800

    over

--- SkipList/skiplist.h
+++ SkipList/skiplist.h
@@ -59,7 +59,7 @@ V Node<K, V>::get_value() const {
 
 template<typename K, typename V>
 void Node<K, V>::set_value(V value) {
-	this->value = value;
+	value = value;
 };
 
 template <typename K, typename V>
@@ -97,8 +97,10 @@ private:
 
     bool equal(K, K);
 
-protected:
+    bool display;
 
+protected:
+   
     virtual std::string node_display(Node<K, V>* node) {
         return "";
     }
@@ -117,6 +119,7 @@ bool SkipList<K, V>::compare(K k1,K k2) {
     }
 }
 
+//=等价于!< && !>
 template<typename K, typename V>
 bool SkipList<K, V>::equal(K k1, K k2) {
     return !compare(k1, k2) && !compare(k2, k1);
@@ -139,9 +142,11 @@ int SkipList<K, V>::insert_element(const K key, const V value) {
     mtx.lock();
     Node<K, V>* current = this->_header;
 
+    //存取每层要插入位置的前一个点
     Node<K, V>** update = new Node<K, V>*[_max_level + 1];
     memset(update, 0, sizeof(Node<K, V>*) * (_max_level + 1));
 
+    //找到每一层的前一个点（<key的最大值）
     for (int i = _skip_list_level; i >= 0; i--) {
         while (current->forward[i] != NULL && compare(current->forward[i]->get_key(),key)) {
             current = current->forward[i];
@@ -149,18 +154,23 @@ int SkipList<K, V>::insert_element(const K key, const V value) {
         update[i] = current;
     }
 
+    //走到底层往右走一格
     current = current->forward[0];
 
+    //相等说明节点已存在，返回
     if (current != NULL && equal(current->get_key(),key)) {
-        std::cout << "key: " << key << ", exists" << std::endl;
+        if(display) std::cout << "node exists:" << node_display(current) << std::endl;
         mtx.unlock();
         return 1;
     }
 
+    //否则插入
     if (current == NULL || !equal(current->get_key(), key)) {
 
+        //获取最高插入哪一层
         int random_level = get_random_level();
 
+        //如果比已有层数大，先补几层，每层插入位置的前一个位置为header
         if (random_level > _skip_list_level) {
             for (int i = _skip_list_level + 1; i < random_level + 1; i++) {
                 update[i] = _header;
@@ -170,23 +180,29 @@ int SkipList<K, V>::insert_element(const K key, const V value) {
 
         Node<K, V>* inserted_node = create_node(key, value, random_level);
 
+        //插入
         for (int i = 0; i <= random_level; i++) {
             inserted_node->forward[i] = update[i]->forward[i];
             update[i]->forward[i] = inserted_node;
         }
-  /*      std::cout << "Successfully inserted key:" << key << ", value:" << value << std::endl;*/
+        if(display) std::cout << "Successfully inserted node:" << node_display(inserted_node) << std::endl;
         _element_count++;
     }
     mtx.unlock();
     return 0;
 }
 
+//显示底层链表
 template<typename K, typename V>
 void SkipList<K, V>::display_list() {
+    if (!display) {
+        std::cout << "please set display to true and define node_display function" << std::endl;
+        return;
+    }
 
     std::cout << "\n*****Skip List*****" << "\n";
     for (int i = 0; i <= _skip_list_level; i++) {
-        Node<K, V>* node = this->_header->forward[i];
+        Node<K, V>* node = _header->forward[i];
         std::cout << "Level " << i << ": ";
         while (node != NULL) {
             std::cout << node_display(node) << "->";
@@ -209,6 +225,7 @@ void SkipList<K, V>::delete_element(K key) {
     Node<K, V>** update = new Node<K,V>*[_max_level + 1];
     memset(update, 0, sizeof(Node<K, V>*) * (_max_level + 1));
 
+    //找到每层前一个位置
     for (int i = _skip_list_level; i >= 0; i--) {
         while (current->forward[i] != NULL && compare(current->forward[i]->get_key(),key)) {
             current = current->forward[i];
@@ -217,8 +234,10 @@ void SkipList<K, V>::delete_element(K key) {
     }
 
     current = current->forward[0];
+    //如果存在这个点
     if (current != NULL && equal(current->get_key(),key)) {
 
+        //遍历每层，若有这个点则删掉，否则Break
         for (int i = 0; i <= _skip_list_level; i++) {
 
             if (update[i]->forward[i] != current)
@@ -226,13 +245,13 @@ void SkipList<K, V>::delete_element(K key) {
 
             update[i]->forward[i] = current->forward[i];
         }
+        if(display) std::cout << "Successfully deleted node: " << node_display(current) << std::endl;
         delete current;
 
+        //可能会把某一层的节点删完，此时层数减1
         while (_skip_list_level > 0 && _header->forward[_skip_list_level] == 0) {
             _skip_list_level--;
         }
-
-        std::cout << "Successfully deleted key " << key << std::endl;
         _element_count--;
     }
     mtx.unlock();
@@ -245,25 +264,27 @@ bool SkipList<K, V>::search_element(K key) {
     //std::cout << "search_element-----------------" << std::endl;
     Node<K, V>* current = _header;
 
+    //找到每层对应节点的前一个位置
     for (int i = _skip_list_level; i >= 0; i--) {
         while (current->forward[i] && compare(current->forward[i]->get_key(), key)) {
             current = current->forward[i];
         }
     }
 
+    //底层往右一格
     current = current->forward[0];
 
     if (current and equal(current->get_key(), key)) {
-        //std::cout << "Found key: " << key << ", value: " << current->get_value() << std::endl;
+        if(display) std::cout << "Found key: " << node_display(current) << std::endl;
         return true;
     }
 
-  /*  std::cout << "Not Found Key:" << key << std::endl;*/
+    std::cout << "Not Found" << std::endl;
     return false;
 }
 
 template<typename K, typename V>
-SkipList<K, V>::SkipList(int max_level):_max_level(max_level), _skip_list_level(0), _element_count(0),_cmp(NULL){
+SkipList<K, V>::SkipList(int max_level):_max_level(max_level), _skip_list_level(0), _element_count(0),_cmp(NULL),display(false){
     K k{};
     V v{};
     this->_header = new Node<K, V>(k, v, _max_level);
@@ -271,7 +292,7 @@ SkipList<K, V>::SkipList(int max_level):_max_level(max_level), _skip_list_level(
 
 template<typename K, typename V>
 template<typename CMP>
-SkipList<K, V>::SkipList(int max_level, CMP cmp):_max_level(max_level), _skip_list_level(0), _element_count(0),_cmp(cmp) {
+SkipList<K, V>::SkipList(int max_level, CMP cmp):_max_level(max_level), _skip_list_level(0), _element_count(0),_cmp(cmp), display(false){
     K k{};
     V v{};
     this->_header = new Node<K, V>(k, v, _max_level);
